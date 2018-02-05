762474381f6a6eedc0ed01a45ea7cb656e11b575
Magomed Magomedov <tiredofcoding@outlook.com>
2018-02-05 18:46:35 +0300

    cgignr

--- Library/Models/User.py
+++ Library/Models/User.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
+    first_name = models.Integer_Field
     last_name = models.CharField(max_length=30)
