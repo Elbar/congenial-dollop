8d3c07547f1d264c3a415c2b355ce0f90347ddaa
ulan <ulannurmamatov@gmail.com>
2018-02-07 01:40:20 +0300

    added audio model

--- catalog/models.py
+++ catalog/models.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 
 from account.models import User
 import uuid # Required for unique book instances
+
+from datetime import datetime, timedelta
 # Create your models here.
 
 
@@ -27,16 +29,26 @@ class Video(Document):
     videoUrl = models.CharField(max_length=150)
 
 
+class Audio(Document):
+    audioUrl = models.CharField(max_length=150)
+
+
 class Book(Document):
     isbn = models.CharField(max_length=50)
     bestseller = models.BooleanField(default=False)
+
+    def delta(self):
+        if self.bestseller:
+            return timedelta(14)
+        else:
+            return timedelta(21)
 
 
 class Checkout(models.Model):
     document = models.OneToOneField(Document, on_delete=models.DO_NOTHING)
     user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
-    since = models.DateField()
-    until = models.DateField()
+    since = models.DateField(auto_now=True)
+    until = models.DateField(default=datetime.now()+Book(document).delta())
 
     def __str__(self):
         return self.document.title + " checked out by " + self.user.first_name + " " + self.user.last_name
