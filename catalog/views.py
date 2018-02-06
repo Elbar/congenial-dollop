98223caf046ba6a277a5652e71fe198b150bfe74
ulan <ulannurmamatov@gmail.com>
2018-02-07 00:17:34 +0300

    fixed gitignore

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from django.shortcuts import render
 from django.views import generic
 from .models import Book, Checkout
-
+from account.models import Librarian
 
 def index(request):
     """
@@ -28,11 +28,14 @@ def book_detail_view(request, pk):
     try:
         book_id = Book.objects.get(pk=pk)
         checkout = Checkout.objects.get(document=book_id)
+        is_librarian = isinstance(request.user, Librarian)
     except Book.DoesNotExist:
         raise Http404("Book does not exist")
 
     return render(
         request,
         'catalog/book_detail.html',
-        context={'book': book_id, 'checkout':checkout}
+        context={'book': book_id, 'checkout':checkout, 'is_librarian': is_librarian}
     )
+
+
