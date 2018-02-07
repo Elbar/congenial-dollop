2a26c564c8fcf9f3703a12fc1450a97dc42e1d29
Magomed Magomedov <tiredofcoding@outlook.com>
2018-02-07 14:54:59 +0300

    Added new API

--- catalog/views.py
+++ catalog/views.py
@@ -4,26 +4,16 @@ from django.views import generic
 from .models import Book, Checkout, Document
 from account.models import User, Patron, Librarian
 
+
 def index(request):
     """
     View function for home page of site.
     """
-    # Generate counts of some of the main objects
-    patron = Patron.objects.create_user(username='john',
-                                        email='jlennon@beatles.com',
-                                        password='glass onion')
-    patron.save()
-    # patron = Patron.objects.get()
-    librarian = Librarian.objects.get()
-    document = Document.objects.get()
-
-    do_checkout(patron,document)
-    num_instances = Checkout.objects.all().count()
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books': num_books, 'num_instances': num_instances},
+        context={'books_count': Book.objects.all.count, 'checkouts_count': Checkout.objects.all.count},
     )
 
 
@@ -31,8 +21,14 @@ class BookListView(generic.ListView):
     model = Book
 
 
-def num_books():
-    return Book.objects.all().count()
+def wipe_all_data():
+    Document.objects.all.delete
+    User.objects.all.delete
+    Checkout.objects.all.delete
+
+
+def print_counts():
+    print('books_count: ' + Book.objects.all.count + ' | checkouts_count: ' + Checkout.objects.all.count)
 
 
 def book_detail_view(request, pk):
@@ -45,7 +41,7 @@ def book_detail_view(request, pk):
     return render(
         request,
         'catalog/book_detail.html',
-        context={'book': book_id, 'checkout':checkout, 'is_librarian': is_librarian}
+        context={'book': book_id, 'checkout': checkout, 'is_librarian': is_librarian}
     )
 
 
@@ -56,8 +52,8 @@ def do_checkout(user, document):
             print("You can't checkout")
         except Checkout.DoesNotExist:
             document.numberOfCopies = document.numberOfCopies - 1
-            document.save()
+            document.save
             new_checkout = Checkout(user=user, document=document)
-            new_checkout.save()
+            new_checkout.save
     else:
         print("Check out is not possible. There no available copies")
